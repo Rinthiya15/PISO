2b522ca0c7b1a68c2135cd6e24b41e5dc4cba4fe
Rinthiya15 <rinthiya1508@gmail.com>
2026-05-18 09:55:02 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,39 +1,40 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, Timer
 
 
 @cocotb.test()
 async def test_counter(dut):
 
-    dut._log.info("Starting Counter Test")
+    dut._log.info("START TEST")
 
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
-
-    # Init
+    # Inputs init FIRST (important)
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-
-    # RESET
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 2)
 
-    # Release reset
+    # Start clock
+    clock = Clock(dut.clk, 10, unit="us")
+    cocotb.start_soon(clock.start())
+
+    # settle time
+    await Timer(1, "ns")
+
+    # reset hold
+    await ClockCycles(dut.clk, 2)
     dut.rst_n.value = 1
 
-    # IMPORTANT: wait 1 cycle BEFORE checking
+    # wait 1 cycle after reset
     await ClockCycles(dut.clk, 1)
     assert int(dut.uo_out.value) == 0
 
-    # Now check counting starts
-    for i in range(1, 11):
-
+    # check counter
+    for i in range(1, 10):
         await ClockCycles(dut.clk, 1)
         value = int(dut.uo_out.value)
 
-        dut._log.info(f"Counter = {value}")
+        dut._log.info(f"COUNT={value}")
 
         assert value == i, f"Expected {i}, Got {value}"
 
