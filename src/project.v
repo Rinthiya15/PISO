caa576844c5c8377ab180506864898bb387d7f12
Rinthiya15 <rinthiya1508@gmail.com>
2026-05-18 11:46:35 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,10 +1,10 @@
 module tt_um_PISO (
     input  wire clk,
-    input  wire rst_n,      // ACTIVE LOW reset (IMPORTANT)
+    input  wire reset,      // ACTIVE HIGH reset (FIXED)
     input  wire ena,
 
     input  wire [7:0] ui_in,
-    output wire [7:0] uo_out,
+    output reg  [7:0] uo_out,
 
     input  wire [7:0] uio_in,
     output wire [7:0] uio_out,
@@ -14,15 +14,18 @@ module tt_um_PISO (
     reg [7:0] shift_reg;
 
     always @(posedge clk) begin
-        if (!rst_n)
+        if (reset)
             shift_reg <= 8'b0;
         else if (ena)
-            shift_reg <= ui_in;      // parallel load
+            shift_reg <= ui_in;   // parallel load
         else
-            shift_reg <= {shift_reg[6:0], 1'b0}; // shift left
+            shift_reg <= {shift_reg[6:0], 1'b0}; // shift
     end
 
-    assign uo_out  = shift_reg;
+    always @(*) begin
+        uo_out = shift_reg;
+    end
+
     assign uio_out = 8'b0;
     assign uio_oe  = 8'b0;
 
